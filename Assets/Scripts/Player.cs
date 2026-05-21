efde6242803911a39a91a1c9f213ae98d74f8b05
softCastella <agelin50@gmail.com>
2026-05-22 00:48:10 +0900

    프로젝트 복구, 플레이어 이동, 배경스크롤 수정 작업
    프로젝트 복구, 플레이어 이동 1차 범위 구현, 스크롤 방향 수정, 속도 조정 작업

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerStub : MonoBehaviour
+{
+    // 이 스크립트는 현재 사용되지 않으며, 실제 플레이어 동작은 Player_.cs에 정의되어 있습니다.
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
